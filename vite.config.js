6ae963c44969bfd7c375accd7394629adc0fae28
nastyazzz1 <zaharova_nastya_2004@mail.ru>
2023-10-12 18:44:57 +0500

    Second

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import {defineConfig} from 'vite';
+import { createHtmlPlugin } from "vite-plugin-html"
+
+const pages = [
+    {
+        name: "index",
+        content: mainPage()
+    },
+    {
+        name: "about",
+        content: aboutPage()
+    },
+]
+
+export default defineConfig({
+    plugins:[
+        createHtmlPlugin({
+            minify: true,
+            pages: pages.map(({ name, content }) => ({
+                filename: `${name}.html`,
+                template: `${name}.html`,
+                injectOptions: {
+                    data: {
+                        injectScript: content,
+                    },
+                },
+            }))
+        })
+    ]
+})
